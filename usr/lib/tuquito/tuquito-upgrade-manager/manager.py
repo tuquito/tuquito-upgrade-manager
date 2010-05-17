859807006e62a9b1c569a3e75a0e347c2a31ab07
Mario Colque <colkito@colkito-desktop.(none)>
2010-05-17 18:03:58 -0300

    mejoras en performance

--- usr/lib/tuquito/tuquito-upgrade-manager/manager.py
+++ usr/lib/tuquito/tuquito-upgrade-manager/manager.py
@@ -19,23 +19,23 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 """
 
+from time import sleep
+sleep(10*60)
+
 import gtk, pygtk
 pygtk.require('2.0')
 from xml.dom import minidom
 from urllib2 import urlopen
-from time import sleep
 import commands, socket, gettext, os, sys
 socket.setdefaulttimeout(10)
 
 # i18n
 gettext.install('tuquito-upgrade-manager', '/usr/share/tuquito/locale')
 
-#sleep(10)
-
 class Manager:
 	def __init__(self):
 		self.glade = gtk.Builder()
-		self.glade.add_from_file('./manager.glade')
+		self.glade.add_from_file('/usr/lib/tuquito/tuquito-upgrade-manager/manager.glade')
 		self.window = self.glade.get_object('window')
 		self.glade.get_object('title').set_markup(_('<big><b>Tiene instalado ') + myVersion + '</b></big>')
 		self.glade.get_object('subtitle').set_markup(_('Ya esta disponible <b>') + newVersion + '</b>!')
@@ -44,7 +44,6 @@ class Manager:
 		self.glade.get_object('lview').set_label(_('Ver las nuevas caracteristicas en la web'))
 		self.glade.get_object('lno').set_label(_('No deseo actualizar mi Tuquito'))
 		self.glade.connect_signals(self)
-		#self.window.show()
 
 	def noUpdate(self):
 		self.glade.get_object('message').set_markup(_('<big><b>Informacion</b></big>'))
@@ -52,7 +51,8 @@ class Manager:
 		self.glade.get_object('message').show()
 
 	def upgrade(self, widget):
-		os.system('tuquitup')
+		os.system('tuquitup &')
+		self.quit(self)
 
 	def download(self, widget):
 		os.system('xdg-open "' + url + '" &')
@@ -61,14 +61,13 @@ class Manager:
 		os.system('xdg-open "' + notes + '" &')
 
 	def no(self, widget):
-		os.system('touch ~/.tuquito/tuquito-upgrade-manager/norun')
-		self.glade.get_object('message').set_markup(_('<big><b>Atención</b></big>'))
-		self.glade.get_object('message').format_secondary_markup(_('Si desea actualizar su Tuquito luego, puede dirigirse a:\n<i>Menú»Sistema»Adminisración»Upgrade Manager</i>'))
+		os.system('touch ' + homePath + 'norun')
+		self.glade.get_object('message').set_markup(_('<big><b>Atencion</b></big>'))
+		self.glade.get_object('message').format_secondary_markup(_('Si desea actualizar su Tuquito luego, puede dirigirse a:\n<i>Menu»Sistema»Adminisracion»Upgrade Manager</i>'))
 		self.glade.get_object('message').show()
 
 	def about(self, widget, data=None):
-		#os.system('/usr/lib/tuquito/garfio/garfio-about.py &')
-		os.system('./upgrade-about.py &')
+		os.system('/usr/lib/tuquito/tuquito-upgrade-manager/upgrade-about.py &')
 
 	def quit(self, widget, data=None):
 		gtk.main_quit()
@@ -111,7 +110,7 @@ for r in releases:
 		url = r.childNodes[7].firstChild.data
 		notes = url + '?r=' + rel + '&c=' + cod
 	except Exception, detail:
-		break
+		exit(1)
 
 	myVersion = 'Tuquito ' + myRelease + ' - ' + myStatus
 	newVersion = 'Tuquito ' + rel + ' - ' + stat
@@ -120,13 +119,10 @@ for r in releases:
 	if cod == myCodename:
 		m = Manager()
 		if float(rel) > float(myRelease):
-			print myRelease + ' > ' + rel
 			m.window.show()
 		elif (myStatus == 'alpha' and stat == 'beta' or stat == 'stable') or (myStatus == 'beta' and stat == 'stable'):
-			print myStatus + ' > ' + stat
 			m.window.show()
 		elif arg != '-d':
-			print "lanzado por usuario"
 			m.noUpdate()
-		gtk.main()
 		break
+gtk.main()
